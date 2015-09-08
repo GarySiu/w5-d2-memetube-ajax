45673c4b929f876597b82ee8868c920323c9f6f3
Gary Siu <Gary@untypeable.com>
2015-09-09 00:58:26 +0100

    Now listing all my videos by AJAX

--- public/js/main.js
+++ public/js/main.js
@@ -14,5 +14,11 @@ function getVideos() {
 }
 
 function appendNewItem(data) {
-  
+
+  $('<div class="video-container">' +
+  '<p class="title"><a href="/videos/' + data.id + '">#' + data.title + '</a></p>'+
+  '<iframe type="text/html" width="640" height="390" src="http://www.youtube.com/embed/' + data.video_id + 
+  '?autoplay=0" frameborder="0"/></iframe>' +
+  '</div>').prependTo('#video-list')
+
 }
